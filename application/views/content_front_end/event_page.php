41e6ee76f065630f798dd72420a350c856e7aa16
C-za <sefirman12@gmail.com>
2017-07-22 13:52:38 +0700

    design template halaman menu event level 1 (DONE)

--- application/views/content_front_end/event_page.php
+++ application/views/content_front_end/event_page.php
@@ -8,7 +8,7 @@
          </div>
       </section>
 
-      <!-- Color Switcher -->
+      <!-- Event Search Feature -->
       <div class="color-switcher" id="choose_color" style="width:300px;"> <a href="#." class="picker_close"><i class="fa fa-angle-right"></i></a>
         <h5>Cari Event . . .</h5>
         <div class="col-md-12">
@@ -80,3 +80,155 @@
         </div>
         <div class="clr"> </div>
       </div>
+
+      <section>
+        <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-12 col-xs-12">
+               		<div class="section listing">
+               			 <div class="col-md-1"></div>
+	                     <article class="col-md-10 col-sm-12 col-xs-12">
+	                        <div class="row">
+	                          
+	                           <div class="grid-1" style="border-top:solid 1px #f44a56; box-shadow: 0 1px 10px #f44a56;">
+	                           	 <a href="#">
+	                              <ul>
+	                              	<div class="col-md-12" style="padding: 10px;">
+		                                 <li class="col-md-4 col-sm-3 col-xs-12 nopadding">
+		                                    <div class="thumb"> <img src="<?php echo base_url('asset/img/11.jpg'); ?>" alt="">
+		                                    </div>
+		                                 </li>
+		                                 <li class="col-md-8 col-sm-9 col-xs-12">
+		                                    <div class="desc post-content">
+		                                       <h5><a href="#">Samsung Galaxy S8 will reportedly launch in April, and LG G6 will suffer for it </a></h5>
+		                                       <ul class="post-tools">
+		                                          <li>by <a href=""><strong> Arsoo</strong> </a></li>
+		                                          <li>  May 10, 2017 </li>
+		                                          
+		                                       </ul>
+		                                       <p> Vestibulum non mauris nisi. Suspen disse suscipit auctor tortor eu ullamcorper. Suspend isse finibus id mi et venenatis. Aenean eu urna et tellus tempus sagittis sed vel mi. Vestibulum elit nunc Aenean eu urna et tellus tempus Aenean eu urna et tellus tempus Aenean eu urna et <a href="" class="readmore"><strong>Read More</strong></a> </p>
+			                                   <hr style="border: solid 1px #f44a56; margin-top: auto; opacity: 0.4;"></hr>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-green btn-small"><i class="ti-money"></i> Berbayar</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-dark-red">Musik</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-orange">Spirituality</a>
+		                                    </div>
+		                                 </li>
+		                            </div>
+	                              </ul>
+	                             </a>
+	                           </div>
+		                       
+	                           <div class="grid-1" style="border-top:solid 1px #f44a56; box-shadow: 0 1px 10px #f44a56;">
+	                           	 <a href="#">
+	                              <ul>
+	                              	<div class="col-md-12" style="padding: 10px;">
+		                                 <li class="col-md-4 col-sm-3 col-xs-12 nopadding">
+		                                    <div class="thumb"> <img src="<?php echo base_url('asset/img/11.jpg'); ?>" alt="">
+		                                    </div>
+		                                 </li>
+		                                 <li class="col-md-8 col-sm-9 col-xs-12">
+		                                    <div class="desc post-content">
+		                                       <h5><a href="#">Samsung Galaxy S8 will reportedly launch in April, and LG G6 will suffer for it </a></h5>
+		                                       <ul class="post-tools">
+		                                          <li>by <a href=""><strong> Arsoo</strong> </a></li>
+		                                          <li>  May 10, 2017 </li>
+		                                          
+		                                       </ul>
+		                                       <p> Vestibulum non mauris nisi. Suspen disse suscipit auctor tortor eu ullamcorper. Suspend isse finibus id mi et venenatis. Aenean eu urna et tellus tempus sagittis sed vel mi. Vestibulum elit nunc Aenean eu urna et tellus tempus Aenean eu urna et tellus tempus Aenean eu urna et <a href="" class="readmore"><strong>Read More</strong></a> </p>
+			                                   <hr style="border: solid 1px #f44a56; margin-top: auto; opacity: 0.4;"></hr>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-green btn-small"><i class="ti-money"></i> Berbayar</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-dark-red">Musik</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-orange">Spirituality</a>
+		                                    </div>
+		                                 </li>
+		                            </div>
+	                              </ul>
+	                             </a>
+	                           </div>
+
+	                           <div class="grid-1" style="border-top:solid 1px #f44a56; box-shadow: 0 1px 10px #f44a56;">
+	                           	 <a href="#">
+	                              <ul>
+	                              	<div class="col-md-12" style="padding: 10px;">
+		                                 <li class="col-md-4 col-sm-3 col-xs-12 nopadding">
+		                                    <div class="thumb"> <img src="<?php echo base_url('asset/img/11.jpg'); ?>" alt="">
+		                                    </div>
+		                                 </li>
+		                                 <li class="col-md-8 col-sm-9 col-xs-12">
+		                                    <div class="desc post-content">
+		                                       <h5><a href="#">Samsung Galaxy S8 will reportedly launch in April, and LG G6 will suffer for it </a></h5>
+		                                       <ul class="post-tools">
+		                                          <li>by <a href=""><strong> Arsoo</strong> </a></li>
+		                                          <li>  May 10, 2017 </li>
+		                                          
+		                                       </ul>
+		                                       <p> Vestibulum non mauris nisi. Suspen disse suscipit auctor tortor eu ullamcorper. Suspend isse finibus id mi et venenatis. Aenean eu urna et tellus tempus sagittis sed vel mi. Vestibulum elit nunc Aenean eu urna et tellus tempus Aenean eu urna et tellus tempus Aenean eu urna et <a href="" class="readmore"><strong>Read More</strong></a> </p>
+			                                   <hr style="border: solid 1px #f44a56; margin-top: auto; opacity: 0.4;"></hr>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-green btn-small"><i class="ti-money"></i> Berbayar</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-dark-red">Musik</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-orange">Spirituality</a>
+		                                    </div>
+		                                 </li>
+		                            </div>
+	                              </ul>
+	                             </a>
+	                           </div>
+	                           
+	                           <div class="grid-1" style="border-top:solid 1px #f44a56; box-shadow: 0 1px 10px #f44a56;">
+	                           	 <a href="#">
+	                              <ul>
+	                              	<div class="col-md-12" style="padding: 10px;">
+		                                 <li class="col-md-4 col-sm-3 col-xs-12 nopadding">
+		                                    <div class="thumb"> <img src="<?php echo base_url('asset/img/11.jpg'); ?>" alt="">
+		                                    </div>
+		                                 </li>
+		                                 <li class="col-md-8 col-sm-9 col-xs-12">
+		                                    <div class="desc post-content">
+		                                       <h5><a href="#">Samsung Galaxy S8 will reportedly launch in April, and LG G6 will suffer for it </a></h5>
+		                                       <ul class="post-tools">
+		                                          <li>by <a href=""><strong> Arsoo</strong> </a></li>
+		                                          <li>  May 10, 2017 </li>
+		                                          
+		                                       </ul>
+		                                       <p> Vestibulum non mauris nisi. Suspen disse suscipit auctor tortor eu ullamcorper. Suspend isse finibus id mi et venenatis. Aenean eu urna et tellus tempus sagittis sed vel mi. Vestibulum elit nunc Aenean eu urna et tellus tempus Aenean eu urna et tellus tempus Aenean eu urna et <a href="" class="readmore"><strong>Read More</strong></a> </p>
+			                                   <hr style="border: solid 1px #f44a56; margin-top: auto; opacity: 0.4;"></hr>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-green btn-small"><i class="ti-money"></i> Berbayar</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-dark-red">Musik</a>
+			                                   <a style="margin-top: -25px;" href="#" class=" btn btn-orange">Spirituality</a>
+		                                    </div>
+		                                 </li>
+		                            </div>
+	                              </ul>
+	                             </a>
+	                           </div>
+
+	                           <div class="pagination-holder">
+	                           <nav>
+	                              <ul class="pagination">
+	                                 <li>
+	                                    <a aria-label="Previous" href=" #"><span aria-hidden="true"><i class="fa fa-angle-left"></i></span></a>
+	                                 </li>
+	                                 <li class="active">
+	                                    <a href=" #">1 <span class="sr-only">(current)</span></a>
+	                                 </li>
+	                                 <li>
+	                                    <a href=" #">2</a>
+	                                 </li>
+	                                 <li>
+	                                    <a href=" #">3</a>
+	                                 </li>
+	                                 <li>
+	                                    <a aria-label="Next" href=" #"><span aria-hidden="true"><i class="fa fa-angle-right"></i></span></a>
+	                                 </li>
+	                              </ul>
+	                           </nav>
+	                        </div>
+	                     </article>
+
+
+	                     <div class="col-md-1"></div>
+                  </div>
+               </div>
+            </div>
+        </div>
+      </section>
